4de3fab9aa109a73167231bd1e755bde222396e1
charith <charithcherry1100gmail.com>
2021-03-29 00:06:47 +0530

    Phase 2 : streamlit visualizations

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 import matplotlib.pyplot as plt
-import lux
+#import lux
 import time
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import seaborn as sns
 
 
 def BloodGroupTypes(data):
@@ -44,10 +48,11 @@ def data_conversion(dataset):
     
 
 def BP_manipulation(data):
-    data.loc[(data['BP'].isnull()) & (data['Weight'].between(20,42,inclusive=True))& (data['Age in yrs'] == 10),'BP']='96/117'
+    data.loc[(data['BP'].isnull()) & (data['Weight'].between(20,42,inclusive=True))& (data['Age in yrs'] <= 10),'BP']='96/117'
     data.loc[(data['BP'].isnull()) & (data['Weight'].between(20,42,inclusive=True))& (data['Age in yrs'] == 11),'BP']='98/119'
     data.loc[(data['BP'].isnull()) & (data['Weight'].between(20,42,inclusive=True))& (data['Age in yrs'] == 12),'BP']='100/121'
     data.loc[(data['BP'].isnull()) & (data['Weight']>=50) & (data['Age in yrs'] >= 13),'BP']='102/124'
+    
     
 
 def column_gen(data):
@@ -77,16 +82,97 @@ def load_data():
 def cache(data):
     return data
        
+def load_start():
+     for percent_complete in range(100):
+         time.sleep(0.01)
+         my_bar.progress(percent_complete + 1)
+def load_exit():
+             my_bar.progress(0)
 
-st.header("HealthyKid")
-st.subheader("Data Analysis")
+
+
+#initialization
+
+st.markdown("# HealthyKid")
+st.markdown("### Data Analysis")
+st.markdown("Data Loading")
+my_bar = st.progress(0)
+load_start()
 data = load_data()
+load_exit()
+st.markdown("Data Cleaning")
+my_bar = st.progress(0)
+load_start()
 data_cleaning(data)
+load_exit()
+st.markdown("Data Conversion")
+my_bar = st.progress(0)
 data_conversion(data)
+load_exit()
+st.markdown("Column Generation")
+my_bar = st.progress(0)
+load_start()
 column_gen(data)
+load_exit()
 data=cache(data)
 st.dataframe(data)
-st.sidebar.subheader("ADDITIONAL FUNCTIONS")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#sidebar
+st.sidebar.title("Visualizations")
+
+
+
+#Number of Students based on School IDs and Gender
+st.sidebar.markdown("###  Number of Students based on School IDs and Gender")
+select = st.sidebar.selectbox('Visualization type', ['Bar plot', 'Pie chart'], key='1')
+school_count = data[' School ID'].value_counts()
+school_count = pd.DataFrame({'School ID':school_count.index, 'Number of Students':school_count.values})
+if not st.sidebar.checkbox("Hide", True,key=1):
+    st.markdown("### Number of Students based on School IDs and Gender")
+    if select == 'Bar plot':
+        fig=px.histogram(data, x=' School ID', color="Sex", barmode='group')
+        st.plotly_chart(fig)
+    else:
+        fig = px.pie(school_count, values='Number of Students', names='School ID')
+        st.plotly_chart(fig)
+
+
+#NULL BP values        
+#st.sidebar.markdown("### Replacing NULL BP values")
+#st.sidebar.button('BP manipulation')
+#st.dataframe(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
